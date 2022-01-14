5c3ff08a8ffbd004646ced6827262c3f6357ed8a
Lukasz Chmielewski <lukchm94@gmail.com>
2022-01-14 14:39:33 +0100

    asd
    asdf

---
+++ img_clf.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
+from keras.preprocessing.image import ImageDataGenerator
+from keras.optimizers import Adam
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+import tensorflow as tf
+
+import os
+
+import numpy as np
